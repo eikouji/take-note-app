f05c07aef834e5aa3b2a8fb82b441c478d766b45
Eiko Ujifusa <eikorouji@gmail.com>
2022-03-31 17:19:53 -0700

    fixed a file path error in script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const express = require('express');
 const fs = require('fs');
 
 const path = ('path');
-const database = require(''.db/db');
+const database = require('../db/db');
 
 // Express and Port establishment //
 
