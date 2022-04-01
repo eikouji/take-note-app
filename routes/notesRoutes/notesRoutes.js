a898893bdfb432540d975d24b8d11da722fe8361
Eiko Ujifusa <eikorouji@gmail.com>
2022-04-01 11:14:45 -0700

    trying to get this to my heroku repo

--- routes/notesRoutes/notesRoutes.js
+++ routes/notesRoutes/notesRoutes.js
@@ -12,8 +12,20 @@ router.get('/notesRoutes', (req, res) => {
     .post(function (req, res) {
         let jsonFilePath = path.join(__dirname, "/db/db.json");
         let newNote = req.body;
-    }
-}
+    
+        // assign highest ID to test note //
+        let highestId = 99;
+        // finds highest id //
+        for (let i = 0; i < database.length; ++1) {
+            let oneNote = database[i];
+
+            if (oneNote.id > highestId) {
+                // highestID will be highest iD in db/note array//
+                highestId = individualNote.id;
+            }
+        }
+
+//
 // new ID to new notes //
 newNote.id = highestId + 1
 // send the note to the db //
